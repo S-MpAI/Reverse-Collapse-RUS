906e6f21b1884407f76701680c15e240722638a2
←«★SeMPaI★»→ <74881427+S-MpAI@users.noreply.github.com>
2025-01-23 13:36:29 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -3,18 +3,18 @@ chcp 65001 >nul
 
 echo === Сброс локальных изменений ===
 git reset --hard
-if %ERRORLEVEL% NEQ 0 (
+if errorlevel 1 (
     echo [Ошибка] Не удалось сбросить изменения.
     pause
-    exit /b %ERRORLEVEL%
+    exit /b 1
 )
 
 echo === Получение обновлений из удаленного репозитория ===
 git pull origin main
-if %ERRORLEVEL% NEQ 0 (
+if errorlevel 1 (
     echo [Ошибка] Не удалось получить обновления.
     pause
-    exit /b %ERRORLEVEL%
+    exit /b 1
 )
 
 echo === Успешное обновление! ===
